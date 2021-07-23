f257326c6838d548d76fb96c70c461814fb7e663
Annepu Hyndhavi <47473752+AnnepuHyndhavi@users.noreply.github.com>
2021-07-23 22:39:08 +0530

    Update moobile_num_info.py

--- moobile_num_info.py
+++ moobile_num_info.py
@@ -13,7 +13,7 @@ import phonenumbers
 
 from phonenumbers import geocoder, carrier, timezone
 
-phone_number = phonenumbers.parse("+917207236442")
+phone_number = phonenumbers.parse("+917207236442")    #ENTER YOUR MOBILE NUMBER
 print(geocoder.description_for_number(phone_number, 'en'))
 print(carrier.name_for_number(phone_number, 'en'))
 print(timezone.time_zones_for_number(phone_number))
